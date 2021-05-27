73899368bc042dc3a44ab46a7ad08702b7d84278
Hiro <s4moozy@gmail.com>
2021-05-27 21:39:20 +0900

    Added Vuex

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,33 +11,11 @@ export default {
   components: {
     PostList
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: '1',
-              title: 'Hello World!', 
-              previewText: 'This is our first Lorem Ipsum Test'
-            },
-            {
-              id: '2', 
-              title: 'Hello World 2!', 
-              previewText: 'This is our first Lorem Ipsum Test 2'
-            }
-          ]
-        })
-      }, 1500)
-
-    }).then(data => {
-      return data
-    }).catch(e => {
-      context.error(new Error())
-    })
-
-  },
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts
+    }
+  }
 }
 </script>
 
